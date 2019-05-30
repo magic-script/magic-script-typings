66e69d503e7664bd6f97e6bb33b0a912f4f41af5
Konrad Piascik <kpiascik@magicleap.com>
2019-05-30 13:42:40 -0400

    Add index.d.ts files as needed. Add script to generate files

---
+++ index.js
@@ -0,0 +1,24 @@
+let fs = require('fs')
+
+let index = 'index.d.ts'
+let luminIndex = './lumin/index.d.ts';
+
+fs.closeSync(fs.openSync(luminIndex, 'w'))
+
+var gatherFilesList = function(fileName, path) {
+    var files = fs.readdirSync(path);
+    files.forEach(file => {
+        let filePath = `${path}/${file}`;
+        let parsedPath = filePath.substring(filePath.indexOf('/') + 1);
+        let text = `/// <reference path='./${parsedPath}' />`;
+        const stats = fs.statSync(filePath);
+        if (stats.isDirectory()) {
+            gatherFilesList(fileName, filePath);
+        } else if (parsedPath !== index) {
+            fs.appendFileSync(fileName, text + '\n');
+        }
+    });
+}
+gatherFilesList(luminIndex, 'lumin');
+fs.closeSync(fs.openSync(index, 'w'));
+fs.writeFileSync(index, '/// <reference path=\'./index.d.ts\' />');

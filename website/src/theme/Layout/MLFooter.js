894cd01bd6af89e4f0360cd4d7f1794a384f3094
Tim Caswell <tcaswell@magicleap.com>
2020-03-27 17:02:17 -0500

    Fix SSR for cookie-control loading

--- website/src/theme/Layout/MLFooter.js
+++ website/src/theme/Layout/MLFooter.js
@@ -3,6 +3,7 @@
 // See LICENSE file in the project root for full license information.
 
 import React from "react";
+
 const options = {
   apiKey: "bd100834001edf98bd5458ce61e48ca552418f9c",
   footerSelector: "#foot-boi"
@@ -10,7 +11,7 @@ const options = {
 
 let loaded = false;
 function loadCookieControl() {
-  if (loaded) return;
+  if (loaded || typeof document === 'undefined') return;
   loaded = true;
   const script = document.createElement('script');
   script.setAttribute('src', 'https://magicleap.com/assets/js/cookie-control-js/cookie-control-vanilla.min.js')
